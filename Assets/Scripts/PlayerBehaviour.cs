5767f9f15a06d74edc7095d19f098412c465e4f3
Pierre BOUTIN <pierre.boutin@viacesi.fr>
2020-05-17 13:43:05 +0200

    ADD : Dual Camera Setup

---
+++ Assets/Scripts/PlayerBehaviour.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerBehaviour : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public Camera camFps;
+    public Camera camOverview;
+    public KeyCode keyCode;
+
+    void Start()
+    {
+        camFps.enabled = true;
+        camOverview.enabled = false;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(keyCode))
+        {
+            camFps.enabled = !camFps.enabled;
+            camOverview.enabled = !camOverview.enabled;
+        }
+    }
+}
